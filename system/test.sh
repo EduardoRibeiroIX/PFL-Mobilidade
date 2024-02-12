361e8f46d56db799076255195c8977f8e21f4dde
EduardoRibeiroIX <eduardo.ribeiro@itec.ufpa.br>
2024-02-12 18:45:29 -0300

    run atualizado

--- system/test.sh
+++ system/test.sh
@@ -1,3 +1,11 @@
+python main.py -data fmnist -m cnn -algo FedAvg -gr 100 -nc 58  -did 0 -pow True 2>&1 | tee "command1.log"
+    
+    cat "command1.log" > "log_Power-of-Choice.txt"
+
+    mv saida.txt "Power-of-Choice.txt"
+    mv Power-of-Choice.txt log_Power-of-Choice.txt ./saidas/
+
+
 cdr_values=(0.16 0.33 0.50)
 jr_values=(0.15 0.20 0.25)
 for cdr_value in "${cdr_values[@]}"; do
